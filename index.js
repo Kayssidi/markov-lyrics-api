b7920e2ba27ea82e41084001f814fb5aa8ae8c87
kayssidi <kdo@gm.com>
2018-07-09 21:23:07 +0200

    fetch songs lyrics and info

--- index.js
+++ index.js
@@ -8,5 +8,12 @@ module.exports = async () => {
 	const album = await lyricist.album(albumid, { fetchTracklist: true } );
 	const trackListID = album.tracklist.map( song => song.id );
 	
-	return trackListID;
+	// for each songs of the album tracklist, map song info and lyrics
+	const songs = await Promise.all(
+		trackListID.map(songID => {
+				return lyricist.song(songID, { fetchLyrics: true }).catch( () => { return false; } )
+		})
+	);
+	
+	return songs;
 }
