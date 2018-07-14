78ec3fe86c34407167a8b014a82685438c1e7900
kayssidi <kdo@gm.com>
2018-07-14 21:10:06 +0200

    handle deferred application installation on mobile (PWA)

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -5,15 +5,19 @@ import Card from 'grommet/components/Card';
 import Box from 'grommet/components/Box';
 import Layer from 'grommet/components/Layer';
 import Paragraph from 'grommet/components/Paragraph';
+import Toast from 'grommet/components/Toast';
 
 import laCautionImg from '../lacaution.jpg';
 
 import CloudIcon from 'grommet/components/icons/base/Like';
+import AddIcon from 'grommet/components/icons/base/Add';
 import Spinning from 'grommet/components/icons/Spinning';
 
 export default class Index extends React.Component {
   
+  //-----------------------------
   constructor(props)
+  //-----------------------------
   {
     super(props);
     this.state =
@@ -22,13 +26,33 @@ export default class Index extends React.Component {
       dataLoaded : false,     // has lyrics data been loaded
       jsonLyrics : undefined, // hold loaded lyrics string or undefined
       error : undefined,      // undefined or hold error description
+      displayDeferredPrompt : false,  // show personalized button instead of integrated popup
+      showInstallToast : false,       // does this app has been installed successfully ?
     };
     
     this.onShowLyricsLayer = this.onShowLyricsLayer.bind(this);
     this.onCloseLyricsLayer = this.onCloseLyricsLayer.bind(this);
+    
+    this.handleBeforeInstallPrompt = this.handleBeforeInstallPrompt.bind(this);
+    this.handleAddToDesktop = this.handleAddToDesktop.bind(this);
+    this.deferredPrompt = null;
   }
   
+  //-----------------------------
+  componentDidMount() {
+  //-----------------------------
+     window.addEventListener('beforeinstallprompt', this.handleBeforeInstallPrompt);
+  }
+  
+  //-----------------------------
+  componentWillUnmount() {
+  //-----------------------------
+     window.removeEventListener('beforeinstallprompt', this.handleBeforeInstallPrompt);
+  }
+  
+  //-----------------------------
   onShowLyricsLayer()
+  //-----------------------------
   {
     this.setState( {showLayer:true} );
     
@@ -38,17 +62,56 @@ export default class Index extends React.Component {
         .catch(error => this.setState({ jsonLyrics: undefined, error: error, dataLoaded: false, showLayer:false }));
   }
   
+  //-----------------------------
   onCloseLyricsLayer()
+  //-----------------------------
   {
     this.setState( {showLayer:false, dataLoaded:false} );
   }
   
+  // called on component mounted -> store the deferred prompt event, in order to be processed later by the user (\see handleAddToDesktop)
+  //-----------------------------
+  handleBeforeInstallPrompt(e)
+  //-----------------------------
+  {
+    e.preventDefault();
+    this.setState( {displayDeferredPrompt: true} );
+    this.deferredPrompt = e;
+  }
+  
+  // user callback -> when called, show the browser prompt that allo user to install the app
+  //-----------------------------
+  handleAddToDesktop()
+  //-----------------------------
+  {
+    // Show the prompt
+    this.deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    this.deferredPrompt.userChoice
+                  .then((choiceResult) => {
+                    if (choiceResult.outcome === 'accepted') {
+                      console.log('User accepted the A2HS prompt');
+                      this.setState( {showInstallToast:true, displayDeferredPrompt:false});
+                    } else {
+                      console.log('User dismissed the A2HS prompt');
+                    }
+                    this.deferredPrompt = null;
+                  });
+  }
+  
+  //-----------------------------
   render() {
+  //-----------------------------
     const remixButton = <Button label='Remix' icon={<CloudIcon />} onClick={ () => this.onShowLyricsLayer() }/>;
+    const installButton = <Button label='Installer' icon={<AddIcon />} onClick={ () => this.handleAddToDesktop() }/>
+    const installToast = "L'application a été installée avec succès sur votre appareil!"
+    
     return (
       <Box>
       <Section pad='large' justify='around' align='center' full='vertical'>
   
+        { this.state.displayDeferredPrompt ? [installButton]:null }
+          
         <Card thumbnail={laCautionImg} label='Peines de Maures / Arc-En-Ciel Pour Daltoniens' heading='La Caution' description={remixButton}/>
         
         <Layer align="center" closer={true} hidden={!this.state.showLayer} overlayClose={true} onClose={ () => this.onCloseLyricsLayer() }>
@@ -59,6 +122,12 @@ export default class Index extends React.Component {
           }
           </Box>
         </Layer>
+        
+        { this.state.showInstallToast ?
+          <Toast status='ok' onClose={ () => { this.setState( {showInstallToast:false} )} }>
+            {installToast}
+          </Toast>:null
+        }
 
       </Section>
       </Box>
