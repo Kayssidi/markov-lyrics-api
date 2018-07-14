d3c311a1501bd2cb03cfbe4b5fd789867d9365fb
kayssidi <kdo@gm.com>
2018-07-14 20:38:05 +0200

    add basic single app page: a card, an image and a button. remove unused component from starter

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,12 +1,31 @@
 import React from 'react'
-import Login from '../components/Login'
+import Section from 'grommet/components/Section';
+import Button from 'grommet/components/Button';
+import Card from 'grommet/components/Card';
+import Box from 'grommet/components/Box';
+
+import laCautionImg from '../lacaution.jpg';
+
+import CloudIcon from 'grommet/components/icons/base/Like';
 
 export default class Index extends React.Component {
+  
+  constructor(props)
+  {
+    super(props);
+    this.state = { };
+  }
+  
   render() {
+    const remixButton = <Button label='Remix' icon={<CloudIcon />} />;
     return (
-      <div>
-        <Login />
-      </div>
+      <Box>
+      <Section pad='large' justify='around' align='center' full='vertical'>
+  
+        <Card thumbnail={laCautionImg} label='Peines de Maures / Arc-En-Ciel Pour Daltoniens' heading='La Caution' description={remixButton}/>
+
+      </Section>
+      </Box>
     )
   }
 }
